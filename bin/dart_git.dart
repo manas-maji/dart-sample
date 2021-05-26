8e512515b1dff95f0fb1762ff08e4f2f33ed9fe2
manas-maji <manas.maji240@gmail.com>
2021-05-26 14:45:58 +0530

    initial commit

---
+++ bin/dart_git.dart
@@ -0,0 +1,5 @@
+import 'package:dart_git/calculator.dart' as calculator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${calculator.calculate()}!');
+}
---
+++ lib/calculator.dart
@@ -0,0 +1,10 @@
+int calculate() {
+  return 6 * 7;
+}
+
+enum OPERATION {
+  ADD,
+  SUBTRACT,
+  MULTIPLY,
+  DIVIDE,
+}
